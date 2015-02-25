a9e8a0ea3983b07c1214bd90008b993243917033
Ricky <ricky@dius.com.au>
2015-02-26 09:58:53 +1100

    Initial Commit

---
+++ quizme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quizme
+//
+//  Created by Ricky Yim on 26/02/2015.
+//  Copyright (c) 2015 dius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
